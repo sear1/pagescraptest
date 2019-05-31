6cffbc7b92ba853ea0650271eaf99d0a3a450e69
sear1 <46456494+sear1@users.noreply.github.com>
2019-05-30 21:27:33 -0700

    updated

---
+++ pagescrp2.py
@@ -0,0 +1,30 @@
+'''
+INPUT: Alter title to grab.
+
+OUTPUT: Grabs title of website.
+
+'''
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def get_title(url):
+    try:
+        html_ = urlopen(url)
+    except HTTPError as err:
+        return None
+    try:
+        bs_obj = BeautifulSoup(html_.read(), "lxml")
+        title = bs_obj.body.h1
+    except AttributeError as err:
+        return None
+    return title
+
+
+title_ = get_title("http://www.pythonscraping.com/pages/page1.html")
+if title_ == None:
+    print("Title could not be found")
+else:
+    print(title_)
